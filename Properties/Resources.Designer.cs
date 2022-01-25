884699161be156da81236215a2a7eebab5f94243
DJZK <emperorxizzle99@gmail.com>
2022-01-26 02:47:12 +0800

    Fixed the title of Microsoft Teams named Zoom

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace Project_Excelsior.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to release v3.0.20.4.
+        ///   Looks up a localized string similar to release v3.0.21.5.
         /// </summary>
         internal static string appVersion {
             get {
